a502c7be99625888d477dd3fd7138bdc507d7457
m4rllon <marllonpc33@gmail.com>
2025-10-26 17:50:45 -0300

    feat: add basic visualization with Solara

--- code/src/main.py
+++ code/src/main.py
@@ -1,5 +1,37 @@
 from RPGModel import RPGModel
+from mesa.visualization import SolaraViz, SpaceRenderer, make_plot_component
+from mesa.visualization.components import AgentPortrayalStyle
+
+def agent_portrayal(agent):
+    return AgentPortrayalStyle(color="tab:orange", size=50)
+
+model_params = {
+    "n": {
+        "type": "SliderInt",
+        "value": 50,
+        "label": "Number of agents:",
+        "min": 10,
+        "max": 100,
+        "step": 1,
+    },
+    "width": 10,
+    "height": 10,
+}
 
 if __name__ == "__main__":
     modelo_rpg = RPGModel()
-    modelo_rpg.step()
+    
+    renderer = SpaceRenderer(model=modelo_rpg, backend="matplotlib").render(
+        agent_portrayal=agent_portrayal
+    )
+
+    page = SolaraViz(
+        modelo_rpg,
+        renderer,
+        components=[],
+        model_params=model_params,
+        name="RPG Model",
+    )
+    # This is required to render the visualization in the Jupyter notebook
+    page
+    
