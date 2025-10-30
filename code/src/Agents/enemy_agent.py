abfa820c563b7c53dbe9b92755505429d60b7449
João Pedro Morbeck <jpmorbeck45@gmail.com>
2025-10-30 18:46:50 -0300

    feat: Alterando agente 'enemy_agent' e criando os mocks

--- code/src/Agents/enemy_agent.py
+++ code/src/Agents/enemy_agent.py
@@ -1,90 +1,112 @@
+import random
 from Interfaces.IBDI_Agent import IBDI_Agent
+from Beliefs.SurvivePlanLogic import SurvivePlanLogic
+from utils.move_to_agent import move_to_agent
 
 class Enemy_Agent(IBDI_Agent):
     """
-    Um agente que representa um personagem inimigo com lógica BDI.
+    Um agente que representa um inimigo comum com lógica BDI.
     """
-    def __init__(self, model, life, defense, nome="Animal"):
-        # Inicializa a interface IBDI_Agent (que inicializa mesa.Agent)
+    def __init__(
+        self,
+        model,
+        cell,
+        beliefs
+        ):
         super().__init__(model)
-        
-        # --- Atributos Básicos do RPG ---
-        self.nome = nome
-        self.life = life
-        self.defense = defense
-        self.is_alive = True if self.life > 0 else False
-
-        self.beliefs['my_health'] = self.life
-        self.beliefs['my_stats'] = {
-            'defense': self.defense
+        self.cell = cell
+        self.type = 'COMMON_ENEMY'
+        self.plan_library = {
+            'SURVIVE': SurvivePlanLogic()
         }
 
-    def process_messages(self, messages):
-        """
-        Processa uma lista de mensagens recebidas.
-        """
-        print(f"  > Agente {self.unique_id} ({self.nome}): Processando {len(messages)} mensagem(ns)...")
-        
-        for msg in messages:
-            sender = msg['sender']
-            content = msg['content']
-            
-            if content['type'] == 'attack':
-                attack_power = content['value']
-                print(f"  > Agente {self.unique_id} ({self.nome}): Recebi um ataque de {sender} com poder {attack_power}.")
-                
-                # Cálculo do dano (Ataque - Defesa)
-                damage_taken = attack_power - self.defense
-                
-                # Garante que o dano não seja negativo
-                if damage_taken < 0:
-                    damage_taken = 0
-                
-                print(f"  > Agente {self.unique_id} ({self.nome}): Meu HP era {self.life}. Sofri {damage_taken} de dano (Defesa: {self.defense}).")
-                self.life -= damage_taken
-                
-                if self.life <= 0:
-                    self.is_alive = False
-                    print(f"  > Agente {self.unique_id} ({self.nome}): Fui derrotado!")
-            else:
-                print(f"  > Agente {self.unique_id} ({self.nome}): Recebi mensagem desconhecida de {sender}: {content['type']}")
+        self.beliefs = beliefs
+        self.desires = ['SURVIVE']
+        self.intention = None
 
-    def introduce_yourself(self):
-        print(f"--- Apresentação do inimigo: {self.unique_id} ({self.nome}) ---")
-        if self.is_alive:
-            print(f"  Vida: {self.life}")
+    def move_to_target(self, target_coordinate, displacement=1):
+        h = self.model.grid.height
+        l = self.model.grid.width
+        pos_a = self.cell.coordinate
+        pos_b = target_coordinate
+        
+        new_position = move_to_agent(
+            h=h,
+            l=l,
+            ax=pos_a[0],
+            ay=pos_a[1],
+            bx=pos_b[0],
+            by=pos_b[1],
+            max_step=displacement
+        )
+        
+        new_cell = next(iter(self.model.grid.all_cells.select(
+            lambda cell: cell.coordinate == new_position
+        )))
+        
+        if new_cell.is_empty:
+            self.cell = new_cell
+            return True
         else:
-            print("  Status: Derrotado.")
-        print("-" * (20 + len(self.nome) + len(str(self.unique_id))))
-    
+            return False
+
+    def flee_and_heal(self):
+        for step in range(self.beliefs['displacement']):
+            neighbor = self.cell.neighborhood.select_random_cell()
+            while not self.move_to_target(neighbor.coordinate):
+                neighbor = self.cell.neighborhood.select_random_cell()
+        self.beliefs['hp'] += random.randint(2, 4)
+        
+    def attack_enemy(self):
+        enemyAgent = self.beliefs['target']
+        
+        damage = self.beliefs['att'] - enemyAgent.beliefs['def']
+        newHpEnemy = enemyAgent.beliefs['hp'] - max(0, damage)
+        
+        if newHpEnemy <= 0:
+            enemyAgent.beliefs['is_alive'] = False
+            enemyAgent.beliefs['hp'] = 0
+        else:
+            enemyAgent.beliefs['hp'] = newHpEnemy
+        print(f'ATAQUE REALIZADO [{self.unique_id} atacou {self.beliefs['target'].unique_id}]\n----> DANO CAUSADO: {damage}')
+
+    # -------- BDI -------- #
     def update_beliefs(self):
-        """ 
-        Fase 1 do BDI: Perceber.
-        Atualiza crenças internas (saúde) e percebe o ambiente (mensagens).
-        """
-        # 1. Perceber estado interno
-        self.beliefs['my_health'] = self.life
-        
-        # 2. Perceber ambiente externo (Checar "Correio")
-        mensagens_recebidas = self.model.get_messages(self.unique_id)
-        
-        if mensagens_recebidas:
-            # Se houver mensagens, processa-as
-            self.process_messages(mensagens_recebidas)
-            
-        # 3. Atualizar estado de vida (após processar dano)
-        if self.life <= 0 and self.is_alive:
-            self.is_alive = False
-            self.beliefs['my_health'] = 0
+        pass
 
     def deliberate(self):
-        pass
+        self.intention = self.plan_library[self.desires[0]].get_intention(self)
 
     def execute_plan(self):
-        pass
+        match self.intention:
+            case 'CURAR':
+                self.flee_and_heal()
+                return
+            
+            case 'ATACAR INIMIGO':
+                self.attack_enemy()
+                return
+            
+            case 'APROXIMAR-SE':
+                print(f'POSIÇÃO: {self.cell.coordinate}')
+                self.move_to_target(
+                    self.beliefs['target'].cell.coordinate,
+                    self.beliefs['displacement']
+                )
+                print(f'POSIÇÃO NOVA: {self.cell.coordinate}')
+                return
+            
+            case 'FUGIR':
+                self.flee_and_heal()
+                return
+            
+            case _:
+                pass
 
     def step(self):
-        print(f"Executando step do animal...")
+        print(f"Executando step do inimigo...")
         self.update_beliefs()
-        # self.deliberate()
-        # self.execute_plan()
+        self.deliberate()
+        self.execute_plan()
+        print(f'INTENÇÃO [{self.unique_id}]: {self.intention}')        
+        print(f'AGENTE [{self.unique_id}]: {self.beliefs}')
