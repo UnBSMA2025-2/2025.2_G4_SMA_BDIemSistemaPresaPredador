ec11bd303b4b7ea75226572975672faf7017461a
m4rllon <marllonpc33@gmail.com>
2025-11-06 07:32:22 -0300

    test: add unit tests for SurvivePlanLogic and get_distance

--- code/src/utils/get_distance.py
+++ code/src/utils/get_distance.py
@@ -1,12 +1,13 @@
-import math
 def get_distance(x1: int, y1: int, x2: int, y2: int) -> int:
     """
     Calcula a distância de Manhattan (em "passos" ou "blocos") 
     entre dois pontos em um grid.
     
     Args:
-        ponto_a (tuple): Uma tupla (x1, y1) com as coordenadas do ponto A.
-        ponto_b (tuple): Uma tupla (x2, y2) com as coordenadas do ponto B.
+        x1 (int): Coordenada X do ponto A.
+        y1 (int): Coordenada Y do ponto A.
+        x2 (int): Coordenada X do ponto B.
+        y2 (int): Coordenada Y do ponto B.
         
     Returns:
         int: A distância entre os pontos A e B (em número de passos).
